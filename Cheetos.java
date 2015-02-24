1eefd951fc66494ed0c8e4ba7f4850c95b7527db
veev <vivyee@bu.edu>
2015-02-23 21:35:56 -0500

    print statements

--- Cheetos.java
+++ Cheetos.java
@@ -13,6 +13,7 @@ public class Cheetos {
 
   public void eat() {
     this.quantity--;
+    System.out.println("That was yummy. Now I have " + this.quantity + " cheetos left!")
   }
 
   public boolean isEmpty() {
