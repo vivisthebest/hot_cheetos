6fee5c17e539fd1e1e145962058bf90aacb90e3d
veev <vivyee@bu.edu>
2015-02-23 21:17:55 -0500

    CVS class

---
+++ CVS.java
@@ -0,0 +1,32 @@
+public class CVS {
+  private String location;
+  private int numHotCheetos;
+
+  public CVS(String location, int numHotCheetos) {
+    this.location = location;
+    this.numHotCheetos = numHotCheetos;
+  }
+
+  public Cheetos buyCheetos(String type, int quantity) {
+    if (type.toLowerCase() == "hot") {
+      if (this.numHotCheetos < quantity) {
+        System.out.println("We don't have that many hot cheetos left.");
+        System.out.println("Here area all the cheetos we have left.");
+        quantity = this.numHotCheetos;
+        this.numHotCheetos = 0;
+        return new Cheetos("hot", quantity);
+      } else {
+        this.numHotCheetos -= quantity;
+        return new Cheetos("hot", quantity);
+      }
+    } else {
+      System.out.println("Don't be an idiot. You want hot cheetos.");
+      return buyCheetos("hot", quantity);
+    }
+  }
+
+  public void restockCheetos(int quantity) {
+    this.numHotCheetos += quantity;
+    System.out.println("The world can run again!");
+  }
+}
